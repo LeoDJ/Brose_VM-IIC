6084cb52bbac7902ef3b960ac2d4c1d470b2b30c
LeoDJ <leandro.spaeth@gmail.com>
2020-09-18 02:25:24 +0200

    add meter display and revolution divisor

--- Brose_VM-IIC_Control/src/src.ino
+++ Brose_VM-IIC_Control/src/src.ino
@@ -22,9 +22,13 @@
 #define DISPLAY_HEIGHT  19
 #define FLIP_TIME       550
 
-#define PULSE_PIN       5
-#define RESET_PIN       4
-#define PULSE_DEBOUNCE  100
+#define PULSE_PIN       13
+#define RESET_PIN       5
+#define PULSE_DEBOUNCE  25
+
+#define COUNTER_DIVISOR 24
+
+#define REVS_PER_METER  67.5
 
 volatile uint32_t counter = 0;
 
@@ -58,7 +62,9 @@ void IRAM_ATTR handlePulse() {
         lastPulse = millis();
         counter++;
         newCounterValue = true;
-        Serial.println(counter);
+        Serial.print(counter);
+        Serial.print(" - ");
+        Serial.println(counter / COUNTER_DIVISOR);
     }
 }
 
@@ -92,16 +98,20 @@ void drawCounter() {
     flipdot.setFont(&FreeSans12pt7b);
     flipdot.setTextColor(1);
     flipdot.fillScreen(0);
-    char buf[10];
-    snprintf(buf, 10, "%05d", counter);
+    char buf[16];
+    snprintf(buf, 16, "%05d", counter / COUNTER_DIVISOR);
     // flipdot.drawCenteredText(0, 17, buf);
     uint16_t w = flipdot.getTextWidth(buf);
     flipdot.fillRect(0, 0, w + 3, 18, 0); // black background
     flipdot.setCursor(-1, 17);
     flipdot.print(buf);
+    flipdot.setFont();
+    snprintf(buf, 16, "  %05.1fm", counter / REVS_PER_METER / COUNTER_DIVISOR);
+    flipdot.print(buf);
     flipdot.update();
 }
 
+uint32_t lastTimeSliceReset = 0;
 
 void setup() {
     Serial.begin(115200);
@@ -149,7 +159,6 @@ void (*timeSliceFuncs[])() = {  drawCounter,    drawMaker,  drawSpace};
 
 uint8_t timeSliceCount = sizeof(timeSliceFuncs)/sizeof(timeSliceFuncs[0]);
 uint8_t timeSliceIdx = 0;
-uint32_t lastTimeSliceReset = 0;
 
 void loop() {
 
@@ -173,14 +182,18 @@ void loop() {
     }
 
     loopOTA();
-    wifiMulti.run();
+    // wifiMulti.run();
 
     // Serial number input
     while(Serial.available()) {
         char c = Serial.read();
         serialBuf[serialBufIdx] = c;
         if(c == '\r' || c == '\n') {
+            uint32_t prevCounter = counter;
             sscanf(serialBuf, "%d", &counter);
+            if(counter != prevCounter) { // if no number is found scanf leaves the value alone
+                counter *= COUNTER_DIVISOR;
+            }
             newCounterValue = true;
             memset(serialBuf, 0, 128);
             serialBufIdx = 0;
